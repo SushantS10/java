4f587583a7d42657ce390cace109d153665935c2
Sushant S <123403089+SushantS10@users.noreply.github.com>
2025-03-01 09:16:07 +0530

    Create linear.java

---
+++ linear.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main{
+    static void linear(int n,int[]nums)
+    {
+        for (int i=0;i<n;i++)
+        {
+            if(n==nums[i])
+            {
+                System.out.println("Element found at"+i);
+                return;
+                
+            }
+        }
+    }
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int[] nums={12,3,2,22,4,44,5,55,67};
+        System.out.println("enter the number to find the its Index");
+        int n=sc.nextInt();
+        linear(n,nums);
+    }
+}
